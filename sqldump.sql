e76f6c97bf7755e9a12062329b64dc1fc37d1734
pratikto <pratiktoariestyadi@luminalearning.com>
2019-08-13 14:18:17 +0100

    use different set of random names

--- sqldump.sql
+++ sqldump.sql
@@ -7,8 +7,8 @@ CREATE TABLE users (
 
 INSERT INTO users (id,firstName,lastName,favourite_movies)
     VALUES
-(1,'Hayes','Cruz','tt0848228,tt4154756,tt2395427,tt4154796'),
-(2,'Addison','Mcdonald','tt4154756,tt10515848,tt0120575'),
-(3,'Dante','Hammond','tt0287871,tt2975590,tt0103776,tt4116284,tt2313197'),
-(4,'Vivien','Davis','tt0926084,tt0417741'),
-(5,'Nehru','Moss','tt0389860');
+        (1,'Anona','Cruz','tt0848228,tt4154756,tt2395427,tt4154796'),
+        (2,'Camilla','Sayer','tt4154756,tt10515848,tt0120575'),
+        (3,'Ganesh','Zentai','tt0287871,tt2975590,tt0103776,tt4116284,tt2313197'),
+        (4,'Vivien','Straub','tt0926084,tt0417741'),
+        (5,'Bernardita','Bishop','tt0389860');
